3092f6e1cee9f59cc84f446f4596907fd64bce3d
mochammadnizar <75961970+mochammadnizaralbaehaqi@users.noreply.github.com>
2022-10-31 20:13:22 +0700

    Tugas 6

--- app/Models/post.php
+++ app/Models/post.php
@@ -2,19 +2,10 @@
 
 namespace App\Models;
 
-class post
-{
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ],
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-        [
-            "title" => "Judul Post Kedua",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ]
-    ];
+class post extends Model
+{
+    use HasFactory;
 }
---
+++ app/Models/post_.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+class post
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "author" => "Mochammad Nizar Albaehaqi",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
+        ],
+
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Mochammad Nizar Albaehaqi",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
+        ]
+    ];
+}
