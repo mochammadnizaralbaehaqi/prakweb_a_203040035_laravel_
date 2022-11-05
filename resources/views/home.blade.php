8ae4821781f44c7b6e9272a8dfdab743df0f2ec3
mochammadnizar <75961970+mochammadnizaralbaehaqi@users.noreply.github.com>
2022-11-05 21:52:57 +0700

    Tugas 7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,13 @@
 
 @section('container')
     <h1>Halaman Home</h1>
+
+    <div class="card mb-3">
+        <img src="https://source.unsplash.com/1600x900/?nature,water" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
 @endsection
