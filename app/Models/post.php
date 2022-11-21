2ba56236bb001a1811cf66036460a0b1eed6357c
mochammadnizar <75961970+mochammadnizaralbaehaqi@users.noreply.github.com>
2022-11-21 20:45:50 +0700

    UPDATE

--- app/Models/post.php
+++ app/Models/post.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class post extends Model
+class post
 {
-    use HasFactory;
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Mochammad Nizar Albaehaqi",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
+        ],
+
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Mochammad Nizar Albaehaqi",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        // $post = [];
+        // foreach ($posts as $p) {
+        //     if ($p["slug"] === $slug) {
+        //         $post = $p;
+        //     }
+        // }
+
+        return $posts->firstWhere('slug', $slug);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,57 +29,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ],
-
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ],
-
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Mochammad Nizar Albaehaqi",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui maxime sunt ullam veniam officia, distinctio ratione vitae molestiae deleniti perferendis doloribus at dignissimos repellat esse similique debitis iusto tempore, neque dolorum quod! Porro, eos ut beatae optio ipsum cum, odit officia ullam labore quas placeat aperiam! Labore est cupiditate voluptatum, voluptas distinctio corrupti eum, cum impedit maiores illum dolorum atque inventore pariatur, voluptatibus commodi velit sint soluta. Error qui ad, explicabo accusantium sint optio perspiciatis quis iure, magnam placeat ea."
-        ]
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
